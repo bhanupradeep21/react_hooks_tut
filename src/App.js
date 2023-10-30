aebd78558da7eb21c8c802cca92d203203b7d8f1
bhanu <bhanukari572@gmail.com>
2023-10-30 20:53:19 +0530

    useEffect hook completed learning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import ReduceHook from './Components.js/ReduceHook';
+import UseEffectHook from './Components.js/UseEffectHook';
 function App() {
   return (
     <div className="App">
       <ReduceHook/>
+      <UseEffectHook/>
     </div>
   );
 }
--- src/Components.js/UseEffectHook.js
+++ src/Components.js/UseEffectHook.js
@@ -1,6 +1,13 @@
 import React,{ useEffect } from 'react'
 
 export default function UseEffectHook() {
+    useEffect(() => {
+        const fetchdata = async ()=>{
+          console.log("hii");
+          const data = await fetch("https://jsonplaceholder.typicode.com/comments")
+          console.log(data)
+        }
+    })
   return (
     <div>
       
